0932bddae3e5bece87b4de77f8212889a0d2eb73
andersonnaz <anderson_1004@hotmail.com>
2023-08-31 18:46:51 -0300

    refactor: move all protocols to one file

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,4 +1,4 @@
-import { Encrypter } from "../../protocols/encrypter";
+import { Encrypter } from "../../usecases/add-account/db-add-account-protocols";
 import { DbAddAccount } from "./db-add-account";
 
 interface SutTypes {
