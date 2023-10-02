0d4e10d252c8204a170f62855ac1aa3d3b425465
andersonnaz <anderson_1004@hotmail.com>
2023-10-02 16:12:05 -0300

    feat: ensure return an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,13 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account';
+import { AddAccountModel } from '../../../../domain/use-cases/add-account';
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+    async add(accountData: AddAccountModel): Promise<AccountModel> {
+       const accountCollection = await MongoHelper.getCollection('accounts')
+       const result = await accountCollection.insertOne(accountData)
+       const accountId = result.insertedId.id.toString()
+       return Object.assign({}, accountData, {id: accountId})
+    }
+}
